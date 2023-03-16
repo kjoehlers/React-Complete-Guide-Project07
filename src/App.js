311f7a7473118a1554435a2f1c5a4f3900dfc119
Ken Oehlers <kjojr56@gmail.com>
2023-03-15 20:06:27 -0400

    Commit before lesson 157

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
+
   return (
     <div className="app">
-      <h1>Hi there!</h1>
+      <h1>Hi There!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
